7725e64100c736607a782053299a3f64f40b2567
MoonJS <blastingzone@naver.com>
2016-02-11 16:15:11 +0900

    initial Swift Project commit

---
+++ StanfordCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordCalculator
+//
+//  Created by Moon on 2016. 2. 11..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
